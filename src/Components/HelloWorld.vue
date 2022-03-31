1a694620e9478a9dc11527dbc503b7e233c97f30
syedZohaibHussain <zohaib.hussain@planet01.net>
2022-03-31 15:23:48 +0500

    ...

--- src/Components/HelloWorld.vue
+++ src/Components/HelloWorld.vue
@@ -1,22 +1,24 @@
 <template>
 	<div class="row">
 		<div class="col-lg-8 col-md-10 mx-auto">
-			<div class="clearfix">
-			</div>
+			<input type="file" name="" id="" class="form-control" @change="onFileChange" />
+			<div class="clearfix"> </div>
+			<img :src="image" />
 		</div>
 	</div>
 </template>
 
 <script>
+	import ExcelJS from "exceljs";
 	import Repository from "src/Repositories/RepositoryFactory";
 	const PostRepository = Repository.get("posts");
-	
 	export default {
 		name: "Posts",
-		components: { },
+		components: {},
 		data() {
 			return {
-				posts: []
+				posts: [],
+				image: ''
 			};
 		},
 		created() {
@@ -24,9 +26,38 @@
 		},
 		methods: {
 			getPosts: async function() {
-				const { data } = await PostRepository.get();
+				const {
+					data
+				} = await PostRepository.get();
 				this.posts = data;
-			}
+			},
+			onFileChange(e) {
+				var files = e.target.files || e.dataTransfer.files;
+				if (!files.length)
+					return;
+				this.createImage(files[0]);
+			},
+			createImage(file) {
+				// let image = new Image();
+				let reader = new FileReader();
+				reader.onload = async (e) => {
+					const buffer	= Buffer.from(e.target.result, 'base64');
+					const workbook	= new ExcelJS.Workbook();
+					workbook.xlsx.load(buffer).then(_workbook => {
+						console.log(_workbook, 'workbook instance')
+						_workbook.eachSheet((sheet, id) => {
+							sheet.eachRow((row, rowIndex) => {
+								console.log(id, row.values, rowIndex)
+							})
+						})
+					})
+				};
+				reader.readAsDataURL(file);
+				// console.log(image);
+			},
+			removeImage: function() {
+				this.image = '';
+			},
 		}
 	};
 </script>
--- src/Views/Home.vue
+++ src/Views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Starter Pack"/>
+    <HelloWorld />
   </div>
 </template>
 
